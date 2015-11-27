03e218e1f4c55ed7e2348ff6aeb685af0d711b6a
Adriann Sanchez <alka65@hotmail.com>
2015-11-26 22:52:03 -0500

    Sensor real data

--- App/apps/dashboard/views.py
+++ App/apps/dashboard/views.py
@@ -1,11 +1,12 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.template import RequestContext
+from django.contrib.auth import logout
 from .models import *
+import datetime
 import json
 
 @login_required
@@ -32,7 +33,9 @@ def sensor(request):
 def data_sensor(request):
 	response = {}
 	if request.is_ajax():
-		value_sensor = Value.objects.filter(sensor = request.GET['sensor_id'])
+		today = datetime.datetime.now()
+		date = today.strftime('%Y-%m-%d')
+		value_sensor = Value.objects.filter(sensor = request.GET['sensor_id'], date = date)
 		response = list(value_sensor.values('value', 'hour'))
 	else:
 		return HttpResponse('/')
@@ -53,12 +56,65 @@ def send_data_sensor(request):
 			val = last_value.hour+1
 		except Value.DoesNotExist:
 			val = 1
-		sensor_value =Value(sensor = sensor, value = value, hour = val)
+		sensor_value = Value(sensor = sensor, value = value, hour = val)
 		sensor_value.save()
 		response['response'] = val
 	else:
 		return HttpResponse('/')
 	return HttpResponse(json.dumps(response), content_type = 'application/json')
+
+def state_space(request):
+	response = {}
+	sen = []
+	val = []
+	t_val = 0
+	total_value = 0
+	i = 0
+	count = 0
+	space_tag = ''
+	attr = ''
+	today = datetime.datetime.now()
+	date = today.strftime('%Y-%m-%d')
+	if request.is_ajax():
+		space_ = Space.objects.get(pk = request.GET['space'])
+		sensors = Sensor.objects.filter(space = space_)
+		for sensor in sensors:
+			try:
+				values = Value.objects.filter(sensor = sensor.pk, date = date).latest('hour')
+				val.append(values.value)
+				space_tag = str(sensor.space.space_tag)
+				sen.append(values.value)
+				count = count + 1
+			except Value.DoesNotExist:
+				val.append(0)
+		for v in val:
+			t_val += v
+			i = i + 1
+		if count > 0:
+			total_value = t_val / count
+			if total_value == 0:
+				attr = 'white'
+			if total_value > 0:
+				attr = 'green'
+			if total_value > 80:
+				attr = 'yellow'
+			if total_value >= 150:
+				attr = 'red'
+			response['value'] = str(total_value)
+			response['space_tag'] = str(space_tag)
+			response['id'] = str(attr)
+			response['text'] = str('Average value')
+		else:
+			response['value'] = 'None'
+			response['space_tag'] = str(space_.space_tag)
+			response['id'] = str('white')
+			response['text'] = str('Fail found data sensor')
+	else:
+		return HttpResponse('/')
+	return HttpResponse(json.dumps(response), content_type = 'application/json')
+
+def graph_space(request):
+	pass
 
 def logout_user(request):
 	logout(request)
